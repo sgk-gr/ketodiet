7081d52b9b6dc55f7bb09a13872c92b5369fc1d5
sgk-gr <info@sgk.gr>
2026-08-19 21:08:22 +0300

    fix: configure dev server host and port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
-    open: false,
+    strictPort: false,
   },
+  preview: {
+    host: true,
+    port: 3000,
+  }
 });
